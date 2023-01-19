91ede2b457d5b1a41461eb281e0ef55517cab9b4
Phil Critchfield <pjcritchfield@tuta.io>
2023-01-19 18:51:13 -0500

    Automation to deploy the EKS cluster

--- infra/index.js
+++ infra/index.js
@@ -1,10 +1,91 @@
-"use strict";
-const pulumi = require("@pulumi/pulumi");
-const aws = require("@pulumi/aws");
-const awsx = require("@pulumi/awsx");
+import * as pulumi from "@pulumi/pulumi";
+import * as awsx from "@pulumi/awsx";
+import * as eks from "@pulumi/eks"
+import * as kubernetes from "@pulumi/kubernetes"
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("philC-liatrio-bucket");
+// Create a VPC for our cluster.
+const vpc = new awsx.ec2.Vpc("vpc", {});
 
-// Export the name of the bucket
-exports.bucketName = bucket.id;
+// Create an EKS cluster with the default configuration.
+const cluster = new eks.Cluster("cluster", {
+    vpcId: vpc.vpcId,
+    publicSubnetIds: vpc.publicSubnetIds,
+    privateSubnetIds: vpc.privateSubnetIds,
+    nodeAssociatePublicIpAddress: false,
+});
+const eksProvider = new kubernetes.Provider("eks-provider", {
+  kubeconfig: cluster.kubeconfigJson
+})
+
+// Deploy the sample API to AWS EKS.
+const myDeployment = new kubernetes.apps.v1.Deployment(
+  "sample-api",
+  {
+    metadata: {
+      name: "sample-api",
+      namespace: "default"
+    },
+    spec: {
+      replicas: 2,
+      selector: {
+        matchLabels: {
+          app: "api"
+        }
+      },
+      template: {
+        metadata: {
+          labels: {
+            app: "api"
+          }
+        },
+        spec: {
+          containers: [
+            {
+              name: "sample-api",
+              image: "philjim/simple-api:latest",
+              ports: [
+                {
+                  containerPort: 3000
+                }
+              ]
+            }
+          ]
+        }
+      }
+    }
+  },
+  {
+    provider: eksProvider
+  }
+)
+const eksService = new kubernetes.core.v1.Service(
+  "api-service",
+  {
+    metadata: {
+      name: "api-service",
+      namespace: "default"
+    },
+    spec: {
+      type: "LoadBalancer",
+      ports: [
+        {
+          port: 80,
+          targetPort: 3000
+        }
+      ],
+      selector: {
+        app: "api"
+      }
+    }
+  },
+  {
+    provider: eksProvider
+  }
+)
+
+// Export the cluster's kubeconfig.
+export const kubeconfig = cluster.kubeconfig
+// Export the URL for the load balanced service.
+export const url = eksService.status.apply(
+    status => status?.loadBalancer?.ingress[0]?.hostname
+)
